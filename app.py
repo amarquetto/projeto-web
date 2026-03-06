2e5837ea17355c6f1b7f2a2dbc4e8c1463437f28
André Marquetto <andre-marquettoo@hotmail.com>
2026-03-05 21:42:16 -0300

    Commit da aula 05/03/2026 - Professor Ronan Fatec

--- app.py
+++ app.py
@@ -31,5 +31,31 @@ def exibe_cor(cor1):
 def exibe_cor2(cor1, cor2):
     return f'<h1 style="color:{cor1}"> A cor escolhida foi: {cor1} e <b style="color{cor2}">{cor2}</b></h1>'
 
+@app.route('/codigos/<int:ano_nascimento>')
+def codigos(ano_nascimento):
+    ##ano_nascimento = 1988
+    dados = {
+    'nome' : "André Marquetto",
+    'ano_nascimento' : ano_nascimento,
+    'idade' : (2026 - ano_nascimento),
+    'texto' : "Vamos escrever um texto bem longo para que o truncate funcione de acordo modelo legal!",
+    'ativo' : 'erro',
+    'pessoas' : ['Joao', 'Maria', 'Andre', 'Sabrina']
+    }
+    return render_template ('codigos.html', **dados)
+
+
+@app.route ('/home')
+def home():
+    return render_template ('blocos/home.html')
+
+@app.route ('/produtos')
+def produtos():
+    return render_template ('template.html')
+
+@app.route ('/perfil')
+def perfil():
+    return render_template ('template.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
